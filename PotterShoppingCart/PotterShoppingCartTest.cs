ebb5f4fb06384f93d38719eec436471ccb097079
hill <hilliverson@gmail.com>
2017-01-16 23:22:08 +0800

    第一次Commit

---
+++ PotterShoppingCart/PotterShoppingCartTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PotterShoppingCart
+{
+    [TestClass]
+    public class PotterShoppingCartTest
+    {
+        [TestMethod]
+        public void TestBuyOneBook()
+        {
+            var target = new PotterShop();
+            var book = "Potter1";
+            var expected = 100;
+            var actual = target.CaculateMoney(book);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
