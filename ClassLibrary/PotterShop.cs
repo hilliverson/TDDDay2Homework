970a1caec638876b6fe036cc15b9ab4cb1752860
hill <hilliverson@gmail.com>
2017-01-17 21:10:39 +0800

    第二次Commit

---
+++ ClassLibrary/PotterShop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class PotterShop
+    {
+        public PotterShop()
+        {
+        }
+
+        public int CaculateMoney(string book)
+        {
+            if(book.Equals("Potter1")|| book.Equals("Potter2")|| book.Equals("Potter3")|| book.Equals("Potter4")|| book.Equals("Potter5"))
+            {
+                return 100;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
